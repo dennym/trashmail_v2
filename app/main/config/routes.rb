7b6cc022f278435a3a99d65cd465079c87b8c31b
Denny Mueller <denny@dennymueller.de>
2015-08-10 19:29:08 +0200

    move api controller fix createdAt and remove unused route

--- app/main/config/routes.rb
+++ app/main/config/routes.rb
@@ -4,7 +4,6 @@ post "/mail_inbound", controller: 'api', action: 'create'
 
 client '/inbox/{{ tenant }}', controller: 'mail', action: 'index'
 
-client '/mail', controller: 'mail', action: 'index'
 client '/about', action: 'about'
 
 # The main route, this should be last. It will match any params not
--- app/main/models/mail.rb
+++ app/main/models/mail.rb
@@ -1,6 +1,9 @@
 class Mail < Volt::Model
   belongs_to :tenant
 
-  permissions do
+  before_save :add_create_time
+
+  def add_create_time
+    self._createdAt = Time.now
   end
 end
